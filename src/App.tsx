257a3bdf3af6971da2836dce4d7560ad8efe7d65
ognjens <svraka.ra181.2019@uns.ac.rs>
2023-05-14 21:07:52 +0200

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Test from "./pages/Test";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to React Router Tutorial</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
